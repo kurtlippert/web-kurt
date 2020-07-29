c9ac0757a6858d0ae4c5b8cfae34e74ebe0ae2f8
Kurt Lippert <kurtlippert@gmail.com>
2020-07-29 14:10:51 -0400

    more incremental changes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+/* eslint-disable prettier/prettier */
+// ^^ I would like files that render view data to be formatted
+// a bit differently from what prettier wants
 import { Fragment as f, createElement as r } from 'react';
 import { render } from 'react-dom';
 import {
@@ -6,12 +9,12 @@ import {
   section,
   strong,
   p,
-  hr,
   img,
   article,
   figure,
   h2,
 } from 'react-dom-factories';
+import { join } from 'ramda';
 import MePic from './assets/me-pic.jpg';
 
 /**
@@ -34,50 +37,74 @@ const fragment = (...elements: React.ReactNode[]) => r(f, {}, elements);
  * @param content The rest of the app: headers, sidenavs, etc
  */
 const root = (content: React.ReactNode) =>
-  section({ className: 'section' }, div({ className: 'container' }, content));
+  section({ className: 'section' },
+    div({ className: 'container' }, content)
+  );
 
-const header = fragment(
-  article(
-    { className: 'media' },
-    figure(
-      { className: 'media-left' },
-      p({ className: 'image is-128x128' }, img({ src: MePic })),
+const header =
+  div({ className: 'content' },
+  article({ className: 'media' },
+    figure({ className: 'media-left' },
+      p({ className: 'image is-128x128' },
+        img({ className: 'is-rounded', src: MePic })
+      ),
     ),
-    div(
-      { className: 'media-content' },
+    div({ className: 'media-content' },
       h1({ className: 'title' }, 'Kurt Lippert'),
-      p(
-        { className: 'subtitle' },
-        'My first website with ',
-        strong({}, 'Bulma'),
-        '!',
+      p({ className: 'subtitle' },
+        'My first website with ', strong({}, 'Bulma'), '!',
       ),
     ),
   ),
-  // img({ src: MePic, width: 64, height: 64 }),
-  // h1({ className: 'title' }, 'Kurt Lippert'),
-  // p(
-  //   { className: 'subtitle' },
-  //   'My first website with ',
-  //   strong({}, 'Bulma'),
-  //   '!',
-  // ),
-);
+  );
 
-const leftContent = div(
-  { className: 'content' },
-  h2({ className: 'title' }, 'What is Lorem Ipsum?'),
-  p(
-    {},
-    `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-  ),
-);
+const leftBody =
+  div({ className: 'content' },
+    h2({ className: 'title' }, 'What is Lorem Ipsum?'),
+    p({},
+      join('', [
+        'Lorem Ipsum is simply dummy text of the printing and typesetting ',
+        "industry. Lorem Ipsum has been the industry's standard dummy text ",
+        'ever since the 1500s, when an unknown printer took a galley of type ',
+        'and scrambled it to make a type specimen book. It has survived not ',
+        'only five centuries, but also the leap into electronic typesetting, ',
+        'remaining essentially unchanged. It was popularised in the 1960s with ',
+        'the release of Letraset sheets containing Lorem Ipsum passages, and ',
+        'more recently with desktop publishing software like Aldus PageMaker ',
+        'including versions of Lorem Ipsum.',
+      ]),
+    ),
+  );
 
-const rightContent = frag;
+const rightBody =
+  div({ className: 'content is-small' },
+    h2({ className: 'title' }, 'What is Lorem Ipsum?'),
+    p({},
+      join('', [
+        'Lorem Ipsum is simply dummy text of the printing and typesetting ',
+        "industry. Lorem Ipsum has been the industry's standard dummy text ",
+        'ever since the 1500s, when an unknown printer took a galley of type ',
+        'and scrambled it to make a type specimen book. It has survived not ',
+        'only five centuries, but also the leap into electronic typesetting, ',
+        'remaining essentially unchanged. It was popularised in the 1960s with ',
+        'the release of Letraset sheets containing Lorem Ipsum passages, and ',
+        'more recently with desktop publishing software like Aldus PageMaker ',
+        'including versions of Lorem Ipsum.',
+      ]),
+    ),
+  )
+
+const body = 
+  div({ className: 'columns' },
+    div({ className: 'column is-four-fifths' }, leftBody),
+    div({ className: 'column is-one-fifth' }, rightBody)
+  );
+
+// const rightContent = frag;
 
 /**
  * This is the variable that will hold
  */
-const content = fragment(header, hr(), leftContent);
+const content = fragment(header, body);
 
 render(root(content), document.querySelector('main'));
